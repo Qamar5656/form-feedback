bd0c96a572d846b5c59268f97055dd3973ca79a6
Qamar5656 <qamar4157@gmail.com>
2024-08-01 00:21:58 +0500

    form feedback

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import FeedbackForm from './components/FeedbackForm.jsx'
+import './components/FeedbackForm.css'
+
+function App() {
+  return (
+   <>
+ <FeedbackForm/>
+   </>
+  )
+}
+
+export default App
